60d3ed4d5afe795c7128795bd2af17a16d3b7cf2
Duje Basic <duje.basic@reppublika.com>
2023-11-16 11:47:38 +0100

    Initial commit

---
+++ test/react_lambda_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReactLambdaApp from '../lib/react_lambda_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/react_lambda_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReactLambdaApp.ReactLambdaAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
